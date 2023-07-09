b775e6c5fe332082d9079e2f559466a2146c3b25
eya jery <jaryeya2000@gmail.com>
2023-07-09 13:44:06 +0100

    ccc

--- awesome-python-login-model-master/liepin/liepin_login.py
+++ awesome-python-login-model-master/liepin/liepin_login.py
@@ -16,6 +16,12 @@ class Leipin(object):
         self.username = username
         self.password = password
         self.headers = {
+            print('aaaa')
+            print('aaaa')
+            print('aaaa')
+            print('aaaa')
+            print('aaaa')
+            print('aaaa')
             'Accept': 'application/json, text/javascript, */*; q=0.01',
             'Accept-Encoding': 'gzip, deflate, sdch',
             'Accept-Language': 'zh-CN,zh;q=0.8',
@@ -30,7 +36,7 @@ class Leipin(object):
             'HTTP': 'http://120.198.231.88:80',
             # 'HTTP':'http://222.174.71.46:9999',
         }  # 测试所用代理
-        self.session = requests.session()
+        
         self.accountUrl = 'https://passport.liepin.com/h/account'
         self.loginUrl = 'https://passport.liepin.com/h/login.json'
         self.Dir = 'E:\\python\\authcode.jpg'  # authcode folder 根据自己情况选择文件夹
@@ -69,10 +75,18 @@ class Leipin(object):
         response = self.session.post(self.loginUrl, headers=self.headers, data=payload, timeout=10,
                                      proxies=self.proxies, allow_redirects=False)
         return response.status_code, response.text
+    print('bbbb')
+    print('bbbb')
+    print('aaaa')
+    print('aaaa')
+    print('aaaa')
+    print('aaaa')
+    print('aaaa')
+    print('aaaa')
+
 
 
 if __name__ == '__main__':
     userName = input("请输入你的用户名:")
-    password = getpass.getpass("password:")
     lp = Leipin(userName, password)
     print(lp.login())
